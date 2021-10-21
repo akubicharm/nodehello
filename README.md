92eff9ee30c572d51e00a6c1b839536969b6f1ed
大溝桂 <kay@KeiOmizo-MacBook-Pro.local>
2021-10-21 21:05:51 +0900

    create

---
+++ README.md
@@ -0,0 +1,1 @@
+SimpleなNodejs App
---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const os = require('os');
+
+const server = http.createServer((request, response) => {
+    var s = JSON.stringify(os.networkInterfaces(), null, 4);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>hello azure arc</h1>");
+    response.end(s);
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
