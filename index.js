a6553eb79f087c68463bd27caf04c01f37542a2b
大溝桂 <kay@KeiOmizo-MacBook-Pro.local>
2021-10-21 21:39:11 +0900

    update

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const os = require('os');
 
 const server = http.createServer((request, response) => {
     var s = JSON.stringify(os.networkInterfaces(), null, 4);
+    var template =
+    "<body style='background:url(https://azurecomcdn.azureedge.net/cvt-7a762ab48c8d4c8318b59415e9c7995bdb25d6f53e87295f8f08fe57d8820b72/images/page/home/accelerate.png) no-repeat'>" +
+    "<div style='float: left;'><h1>Hello Azure Arc</h1></div>" +
+    "<div style='float: right;'><pre>" + s + "</pre></div>" +
+    "</div>" +
+    "</body>";
+    
+
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>hello azure arc</h1>");
-    response.end("<pre>" + s + "</pre>");
+    response.end(template);
 });
 
 const port = process.env.PORT || 1337;
